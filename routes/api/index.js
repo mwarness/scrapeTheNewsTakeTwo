ab4fcb0d58f3fbf25214d13d2d01f89101ca9627
Mark Warness <markwarness@gmail.com>
2019-10-10 12:23:44 -0400

    fixed routing error

--- routes/api/index.js
+++ routes/api/index.js
@@ -5,7 +5,7 @@
 var router = require("express").Router();
 var fetchRoutes = require("./fetch");
 var noteRoutes = require("./notes");
-var headlineRoutes = require("./headlines");
+var headlineRoutes = require("./Headlines");
 var clearRoutes = require("./clear");
 
 router.use("/fetch", fetchRoutes);
